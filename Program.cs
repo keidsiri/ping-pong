4888669fb988cd686ea138823c2bac5d02e85a34
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-06 16:38:42 -0800

    Add basic code for functionality.
    Co-authored-by : Kate Kiatsiri <keidsiri@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Interface 
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Input a number!");
+      string stringNumber = Console.ReadLine();
+      int intNumber = int.Parse(stringNumber);
+
+      for (int i =1 ; i <= intNumber ; i++)
+      {
+        if ( i % 3 == 0  && i % 5 == 0)
+        {
+          Console.WriteLine("ping-pong");
+        }
+        else if ( i % 3 == 0)
+        {
+          Console.WriteLine("ping");
+        }
+        else if (i % 5 == 0)
+        {
+          Console.WriteLine("pong");
+        }
+        else
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
+  }
+}
